9ce60d96974c31c04270f818d64f46e55e69053a
Jarronb <baileyjarron@gmail.com>
2019-02-16 01:23:49 -0600

    Added git cloning to node start script

--- packer/scripts/start-node-server.sh
+++ packer/scripts/start-node-server.sh
@@ -1,5 +1,12 @@
 #/bin/bash
 
+# Git clone
+cd /home/vagrant
+mkdir repo
+cd repo
+git clone git@github.com:illinoistech-itm/2019-team-09f.git
+
+
 #Start app
 sudo chown vagrant:vagrant /home/vagrant/.pm2/rpc.sock /home/vagrant/.pm2/pub.sock
-pm2 start /home/vagrant/hello.js
+pm2 start /home/vagrant/repo/app/index.js
