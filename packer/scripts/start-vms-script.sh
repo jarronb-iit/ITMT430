29d7d49197d2c7c4c1475f5b6e180c29c11cf1e8
jarronb <baileyjarron@gmail.com>
2019-02-27 04:06:59 -0600

    Removed the nginx-web-server ssh from the start script

--- packer/scripts/start-vms-script.sh
+++ packer/scripts/start-vms-script.sh
@@ -7,5 +7,4 @@ vagrant up --provision
 cd ../nginx-web-server/
 
 vagrant up --provision
-echo "Web application can be viewed here: http://192.168.50.12:80"
-vagrant ssh
+echo "Web application can be viewed here: http://localhost:8080"
