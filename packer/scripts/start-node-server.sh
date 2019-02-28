6f73b12b424c20fe83a074cd94fee417a0489489
jarronb <baileyjarron@gmail.com>
2019-02-28 04:25:40 -0600

    Change node start command to pass in a NODE_ENV var

--- packer/scripts/start-node-server.sh
+++ packer/scripts/start-node-server.sh
@@ -9,6 +9,7 @@ cd /vagrant/repo/app
 sudo npm run npm-install
 
 #Start app
-sudo chown vagrant:vagrant /home/vagrant/.pm2/rpc.sock /home/vagrant/.pm2/pub.sock
-sudo pm2 stop 0
-sudo pm2 start /vagrant/repo/app/index.js
+# sudo chown vagrant:vagrant /home/vagrant/.pm2/rpc.sock /home/vagrant/.pm2/pub.sock
+# sudo pm2 stop 0
+# sudo pm2 start /vagrant/repo/app/index.js
+NODE_ENV=production node /vagrant/repo/app/index.js
