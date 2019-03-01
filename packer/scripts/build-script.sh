641a9fc5c32963963df4e583f90a3174c09ee3b4
Jarron <baileyjarron@gmail.com>
2019-02-28 18:01:36 -0600

    Creatd a script to only add the vms to vagrant

--- packer/scripts/build-script.sh
+++ packer/scripts/build-script.sh
@@ -8,12 +8,6 @@ cd ../vanilla-install/ || exit 1
 
 packer build all-servers.json || exit 1 
 
-cd ../build/ || exit 1
+# Add all vms to Vagrant
+bash ./add-all-vms.sh
 
-# Add servers
-
-vagrant box add ./nginx-web-server-virtualbox.box --name nginx-web-server || exit 1
-
-vagrant box add ./node-application-server-virtualbox.box --name node-application-server || exit 1
-
-vagrant box add ./mongodb-server-virtualbox.box --name mongodb-server || exit 1
