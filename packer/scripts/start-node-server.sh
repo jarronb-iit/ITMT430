30a4a41f720e434ddd32ab2e7a9df8696f14ac26
Jarron <baileyjarron@gmail.com>
2019-03-11 14:33:41 -0500

    Adjusting nose script to pull dev branch

--- packer/scripts/start-node-server.sh
+++ packer/scripts/start-node-server.sh
@@ -3,9 +3,9 @@
 # Git clone
 rm -rf /vagrant/repo
 mkdir /vagrant/repo
-git clone git@github.com:illinoistech-itm/2019-team-09f.git /vagrant/repo/
+git clone -b jarrono-dev git@github.com:illinoistech-itm/2019-team-09f.git /vagrant/repo/
 
-cd /vagrant/repo/app
+cd /vagrant/repo/app/server
 sudo npm run npm-install-linux
 
 #Start app
