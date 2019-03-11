8f908006205efb14b64772c66f1275ae381b0596
Jarron <baileyjarron@gmail.com>
2019-03-11 03:18:45 -0500

    Created npm script to start node server in production mode for linux

--- packer/scripts/start-node-server.sh
+++ packer/scripts/start-node-server.sh
@@ -6,10 +6,11 @@ mkdir /vagrant/repo
 git clone git@github.com:illinoistech-itm/2019-team-09f.git /vagrant/repo/
 
 cd /vagrant/repo/app
-sudo npm run npm-install
+sudo npm run npm-install-linux
 
 #Start app
 # sudo chown vagrant:vagrant /home/vagrant/.pm2/rpc.sock /home/vagrant/.pm2/pub.sock
 # sudo pm2 stop 0
 # sudo pm2 start /vagrant/repo/app/index.js
-NODE_ENV=production node /vagrant/repo/app/index.js
+cd server;
+npm run production-server
