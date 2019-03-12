35e12ef003fe7f2693d2755d142294c35e1e2ccb
Jarron <baileyjarron@gmail.com>
2019-03-12 02:05:41 -0500

    Created routes directory and routing logic

---
+++ app/server/routes/api/user.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+// const passport = require("passport");
+
+// Load Model
+const User = require("../../models/User");
+
+// // Load Validation
+// const validatePostInput = require("../../validation/post");
+
+// @route   GET api/user/test
+// @desc    Tests user route
+// @access  Public
+router.get("/test", (req, res) => {
+  res.json({
+    msg: "User works"
+  });
+});
+
+module.exports = router;
--- app/server/server.js
+++ app/server/server.js
@@ -1,15 +1,26 @@
 const mongoose = require("mongoose");
 const path = require("path");
+const bodyParser = require("body-parser");
 const express = require("express"),
   http = require("http"),
   app = express(),
   server = http.createServer(app);
+
+// Load routes files
+const buyer = require("./routes/api/buyer");
+const property = require("./routes/api/property");
+const seller = require("./routes/api/seller");
+const user = require("./routes/api/user");
 
 // Load Keys
 const keys = require("./config/keys");
 
 let port = keys.port || 5000;
 let webAddress = keys.webAddress;
+
+// Body-Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // MongoDB connection
 mongoose
@@ -28,6 +39,12 @@ mongoose
   .catch(error => {
     console.log(error);
   });
+
+// Use Routes
+app.use("/api/buyer", buyer);
+app.use("/api/property", property);
+app.use("/api/seller", seller);
+app.use("/api/user", user);
 
 app.get("/", (req, res) => {
   res.json({ msg: "Bye world" });
