51685dd2bdde3a08cc63815e91ae0aed83a54d00
Jarron <baileyjarron@gmail.com>
2019-03-14 00:38:59 -0500

    Deleted necessary comment, and reformated the code to remove password from user doc

--- app/server/routes/api/user.js
+++ app/server/routes/api/user.js
@@ -31,7 +31,8 @@ router.post("/", (req, res) => {
     password: req.body.password,
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    phoneNumber: req.body.phoneNumber
+    phoneNumber: req.body.phoneNumber,
+    typeOfUser: req.body.typeOfUser
   });
 
   // Hash Password with Bcryptjs
@@ -47,14 +48,8 @@ router.post("/", (req, res) => {
         .save()
         .then(user => {
           // Takes password out of user object
-          user = {
-            id: user.id,
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            phoneNumber: user.phoneNumber,
-            dateCreated: user.date
-          };
+          const { password, ...formattedUserObj } = user._doc;
+          user = formattedUserObj;
 
           // Create json web token: payload is new user
           jwt.sign(
@@ -69,56 +64,15 @@ router.post("/", (req, res) => {
           );
         })
         .catch(err => {
-          errors = errorFormatter(err);
-          res.status(400).json({ error: errors });
+          if (err.errors) {
+            errors = errorFormatter(err);
+            res.status(400).json({ error: errors });
+          } else {
+            console.log(err);
+          }
         });
     });
   });
 });
 
 module.exports = router;
-
-// (firstName:
-// { ValidatorError: Path `firstName` is required.
-//     at new ValidatorError (C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\error\validator.js:29:11)
-//     at validate (C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\schematype.js:930:13)
-//     at C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\schematype.js:983:11
-//     at Array.forEach (<anonymous>)
-//     at SchemaString.SchemaType.doValidate (C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\schematype.js:939:19)
-//     at C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\document.js:1936:9
-//     at processTicksAndRejections (internal/process/next_tick.js:74:9)
-//   message: 'Path `firstName` is required.',
-//   name: 'ValidatorError',
-//   properties:
-//    { validator: [Function],
-//      message: 'Path `firstName` is required.',
-//      type: 'required',
-//      path: 'firstName',
-//      value: undefined },
-//   kind: 'required',
-//   path: 'firstName',
-//   value: undefined,
-//   reason: undefined,
-//   [Symbol(mongoose:validatorError)]: true },
-// lastName:
-// { ValidatorError: Path `lastName` is required.
-//     at new ValidatorError (C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\error\validator.js:29:11)
-//     at validate (C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\schematype.js:930:13)
-//     at C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\schematype.js:983:11
-//     at Array.forEach (<anonymous>)
-//     at SchemaString.SchemaType.doValidate (C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\schematype.js:939:19)
-//     at C:\Users\baile\Documents\School\ITMT-430\Final\app\server\node_modules\mongoose\lib\document.js:1936:9
-//     at processTicksAndRejections (internal/process/next_tick.js:74:9)
-//   message: 'Path `lastName` is required.',
-//   name: 'ValidatorError',
-//   properties:
-//    { validator: [Function],
-//      message: 'Path `lastName` is required.',
-//      type: 'required',
-//      path: 'lastName',
-//      value: undefined },
-//   kind: 'required',
-//   path: 'lastName',
-//   value: undefined,
-//   reason: undefined,
-//   [Symbol(mongoose:validatorError)]: true } }
