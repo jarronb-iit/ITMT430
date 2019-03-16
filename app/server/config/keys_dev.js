2202d447e905d0c887a2653f2961afb36221ee98
Jarron <baileyjarron@gmail.com>
2019-03-16 16:52:06 -0500

    Installed redis on node sever and added its enviroment varibles to the keys files

--- app/server/config/keys_dev.js
+++ app/server/config/keys_dev.js
@@ -1,12 +1,15 @@
 const dotenvJSON = require("dotenv-json");
 dotenvJSON({ path: "../../packer/vanilla-install/variables.json" });
-
+console.log(process.env);
 module.exports = {
   mongoURI: process.env["database-development-connection"],
   nginxIP: process.env["nginx-web-server-ip"],
   mongodbIP: process.env["database-ip"],
   webAddress: "localhost",
-  jwtSecret: process.env["jwt-secret"]
+  jwtSecret: process.env["jwt-secret"],
+  redisPassword: process.env["redis-server-password"],
+  redisIp: process.env["redis-server-ip"],
+  redisPort: process.env["redis-server-port"]
 };
 
 console.log("ENV = DEVELOPMENT");
