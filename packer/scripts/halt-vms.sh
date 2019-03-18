8ad2188a88c291ddcc64487ec2f1cd4bc16253a9
Jarron <baileyjarron@gmail.com>
2019-03-18 15:36:26 -0500

    Added redis and mongo rep1 to the halt vms script

--- packer/scripts/halt-vms.sh
+++ packer/scripts/halt-vms.sh
@@ -15,10 +15,22 @@ vagrant halt
 
 echo "nginx-web-server shutdown..."
 
-
-
 # Halt mongodb server
 cd ../mongodb-server/
+
+vagrant halt
+
+echo "mongodb server shutdown..."
+
+# Halt mongodb rep1 server
+cd ../mongodb-rep1-server/
+
+vagrant halt
+
+echo "mongodb rep1 server shutdown..."
+
+# Halt redis caching server
+cd ../mongodb-server/redis-caching-server/
 
 vagrant halt
 
