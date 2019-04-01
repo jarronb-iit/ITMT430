e60dd6a857fa0acb5436f15bafc16360420f31e6
Afoggs <fireheartfoggs@gmail.com>
2019-04-01 15:17:43 -0500

    Moved over vonique font to correct path. Also added initial css by Erick and fixed minor bugs. Recreated proper design for login page.

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
 
 //HomePage To do
 // Add OAuth Logins
-// Add Sign Up
+// Add Sign Up redirect functionality
 // Add background Image
 
 
@@ -32,24 +32,28 @@ class App extends Component {
       <div id="loginPage">
 
 
-      <div className="Welcome">
-      <h1>Welcome to Roomie</h1>
-      </div>
-
-      <form onSubmit= {this.handleLogIn}>
-      <label className="usernameBox">
-        Username
-          <input type = "text" name = "username" /*value = {this.state.userName}*/ placeholder = "Enter username"/>
-      </label>
-      <br/>
-      <label className = "passwordBox">
-        Password
-        <input type = "password" name = "password" /*value = {this.state.password}*/ placeholder="Enter password"/>
-      </label>
-      <input type="submit" value= "Submit"/>
+      <form method = "get" action = "@null" autocomplete = "on" onSubmit = {this.handleLogIn}>
+        <p>Welcome to Roomie</p>
+        <div class = "left-arrow"></div>
+      <ol>
+        <li id = "logemail">
+            <input type = "email" name = "email" id ="email" /*value = {this.state.userName}*/ placeholder = "Enter username"/>
+        </li>
+        <li id = "logpass">
+          <input type = "password" name = "password" id="pass" /*value = {this.state.password}*/ placeholder="Enter password"/>
+        </li>
+        <li id = "forgotpass">
+          <a href="#"> Forgot Password?</a>
+        </li>
+        <li id = "login">
+          <input type="submit" className="submit" value= "Login"/>
+          </li>
+        <li id = "signup">
+        <button type="button" className="submit" onclick="location.href = /signup"> Sign Up </button>
+        </li>
+        </ol>
       </form>
-      <br/>
-       <button type="button" id="signUp">Sign Up</button> 
+
 
 
 
