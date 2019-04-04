8e8835b292829625315d4dcb911fc8581bf1e1c6
Jarron <baileyjarron@gmail.com>
2019-04-04 03:48:55 -0500

    Formatted auth route file

--- app/server/routes/api/auth.js
+++ app/server/routes/api/auth.js
@@ -1,41 +1,38 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
-const auth = require("../../middleware/auth");
-
-// const passport = require("passport");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const auth = require('../../middleware/auth');
+const errorsFormatter = require('../../helperFunctions/errorsFormatter');
 
 // Load Model
-const User = require("../../models/User");
-
-// // Load Validation
-// const validatePostInput = require("../../validation/post";
+const User = require('../../models/User');
 
 // @route   GET api/auth/test
 // @desc    Tests auth route
 // @access  Public
-router.get("/test", (req, res) => {
+router.get('/test', (req, res) => {
   res.json({
-    msg: "Auth works"
+    msg: 'Auth works',
   });
 });
 
 // @route   POST api/auth/
 // @desc    Authenticate user
 // @access  public
-router.post("/", (req, res) => {
-  User.findOne({ email: req.body.email }).then(user => {
+router.post('/', (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
     if (!user)
       return res
         .status(400)
-        .json({ errors: [{ message: "Email and/or password is incorrect." }] });
+        .json({ errors: [{ message: 'Email and/or password is incorrect.' }] });
+
     // Compare Password with Bcryptjs Hashed
-    bcrypt.compare(req.body.password, user.password).then(isMatch => {
+    bcrypt.compare(req.body.password, user.password).then((isMatch) => {
       if (!isMatch)
         return res.status(400).json({
-          errors: [{ message: "Email and/or password is incorrect." }]
+          errors: [{ message: 'Email and/or password is incorrect.' }],
         });
 
       // Remove user password from user object
@@ -44,11 +41,11 @@ router.post("/", (req, res) => {
         email: user.email,
         firstName: user.firstName,
         lastName: user.lastName,
-        phoneNumber: user.phoneNumber
+        phoneNumber: user.phoneNumber,
       };
 
       // Create json web token: payload is new user
-      jwt.sign(user, keys.jwtSecret, { expiresIn: "3d" }, (error, token) => {
+      jwt.sign(user, keys.jwtSecret, { expiresIn: '3d' }, (error, token) => {
         // Once jwt is signed, run code
         if (error) throw error;
         res.json({ token: token, user });
@@ -114,7 +111,7 @@ router.post('/user', (req, res) => {
               if (error) throw error;
               res.status(200).json({ token: token, user });
             },
-    );
+          );
         })
         .catch((err) => {
           if (err.errors) {
