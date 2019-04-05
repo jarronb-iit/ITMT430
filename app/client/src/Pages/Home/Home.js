5e8f137b96f3ac90088f77f898858d6d957e12f2
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-05 13:33:01 -0500

    test functionalility of new buttons for tellmepage

--- app/client/src/Pages/Home/Home.js
+++ app/client/src/Pages/Home/Home.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import LoginButtons from '../../components/LoginPageButtons/LoginButtons';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import AboutMeInfo from '../../components/TellMeAboutYou/TellMe';
 import './Home.css';
 
 export default class Home extends Component {
   state = {
-    changeToSignUp: true
+    changeToSignUp: true,
+    changeToAboutMe: true
   };
   changeVisibility = () => {
     if (this.state.changeToSignUp) {
       this.setState({
         changeToSignUp: !this.state.changeToSignUp
       });
-    } else {
-      this.setState({ changeToSignUp: true });
     }
+    // else if (this.state.changeToAboutMe) {
+    //   this.setState({
+    //     changeToAboutMe: !this.changeToAboutMe
+    //   });
+    // }
+    else {
+      this.setState({
+        changeToSignUp: true
+      });
+    }
+    //  else {
+    //   this.setState({
+    //     changeToAboutMe: true
+    //   });
+    // }
   };
   render() {
     let exeComp;
@@ -24,12 +39,17 @@ export default class Home extends Component {
         <LoginButtons loginRequest={this.changeVisibility} />
       );
     } else {
-      exeComp = (
-        //Replace header with new logo image of roomie.
-        <SignUpForm returnToLogin={this.changeVisibility} />
-      );
+      if (this.state.changeToAboutMe) {
+        exeComp = (
+          <SignUpForm
+            returnToLogin={this.changeVisibility}
+            changeAboutMe={this.changeVisibility}
+          />
+        );
+      } else {
+        exeComp = <AboutMeInfo />;
+      }
     }
-
     return <div>{exeComp}</div>;
   }
 }
