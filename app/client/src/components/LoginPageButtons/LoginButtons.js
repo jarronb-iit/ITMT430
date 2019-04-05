612c028cf33804ab584299d6a629e0ade916126d
ecabrera796 <ecabrera796@gmail.com>
2019-04-05 01:47:36 -0500

    imported Grid

--- app/client/src/components/LoginPageButtons/LoginButtons.js
+++ app/client/src/components/LoginPageButtons/LoginButtons.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 export default function LoginButtons(props) {
   return (
