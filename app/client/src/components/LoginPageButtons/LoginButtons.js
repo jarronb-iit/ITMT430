dd9801aa0850e865e29eb38545786fe2779658d4
ecabrera796 <ecabrera796@gmail.com>
2019-04-05 01:47:59 -0500

    put buttons inside Grid

--- app/client/src/components/LoginPageButtons/LoginButtons.js
+++ app/client/src/components/LoginPageButtons/LoginButtons.js
@@ -6,11 +6,17 @@ export default function LoginButtons(props) {
   return (
     //Replace header with new logo image of roomie.
     <div id='homePage'>
-      <header> Roomie </header>
-      <button id='login'>Login</button>
-      <button id='signUp' onClick={props.loginRequest}>
+      <header id='title'> Roomie </header>
+      <form>
+        <Grid xs={12}>
+          <Button id='login'>Login</Button>
+        </Grid>
+        <Grid xs={12}>
+          <Button id='signUp' onClick={props.loginRequest}>
         Sign Up
-      </button>
+          </Button>
+        </Grid>
+      </form>
     </div>
   );
 }
