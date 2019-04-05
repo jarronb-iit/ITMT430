7fee185914d3e18bb85eae5d251ea4a66479b497
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-04 20:41:58 -0500

    This can now access both components

---
+++ app/client/src/Pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import LoginButtons from '../../components/LoginPageButtons/LoginButtons';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import './Home.css';
+
+export default function Home(props) {
+  const handleLogIn = event => {
+    if (this.state.value != 'Antoine');
+    {
+      alert('Your username or password is incorrect');
+      event.preventDefault();
+    }
+  };
+  return (
+    //Replace header with new logo image of roomie.
+    <SignUpForm />
+  );
+}
