5cf08a549927b0eee815366c7b7c7358391b0b24
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-04 20:42:27 -0500

    create 2 components for sign up and log in

---
+++ app/client/src/components/LoginPageButtons/LoginButtons.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+export default function LoginButtons(props) {
+  return (
+    //Replace header with new logo image of roomie.
+    <div id='homePage'>
+      <header> Roomie </header>
+      <button>Login</button>
+      <button>Sign Up</button>
+    </div>
+  );
+}
