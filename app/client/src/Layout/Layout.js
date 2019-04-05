01d214efff4154f036cda8aa3b5883742d1d0897
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-04 21:33:46 -0500

    Fix CSS call that was deleted in prior commit

--- app/client/src/Layout/Layout.js
+++ app/client/src/Layout/Layout.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import SampleContainer from '../containers/SampleContainer/SampleContainer';
-import LoginButtons from '../components/LoginPageButtons/LoginButtons';
-import HomePage from '../Pages/Home/Home';
+import Home from '../Pages/Home/Home';
 import './Layout.css';
 // import { Route, Switch } from "react-router-dom";
 
 export default class Layout extends Component {
   render() {
-    return <LoginButtons />;
+    return <Home />;
   }
 }
