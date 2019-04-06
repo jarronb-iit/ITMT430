432d35826f7f297e39e188e714e06f7e3395eaa0
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-05 22:56:20 -0500

    test button functionality it still fails to call

--- app/client/src/Pages/Home/Home.js
+++ app/client/src/Pages/Home/Home.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import LoginButtons from '../../components/LoginPageButtons/LoginButtons';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import AboutMeInfo from '../../components/TellMeAboutYou/TellMe';
-//import LoggingInForm from '../../components/LoggingInForm/LoggingInForm';
-
+import LoginIn from '../../components/LoggingInForm/LoggingInForm';
 import './Home.css';
 
 export default class Home extends Component {
@@ -16,37 +15,38 @@ export default class Home extends Component {
       this.setState({
         changeToSignUp: !this.state.changeToSignUp
       });
-    }
-    // else if (this.state.changeToAboutMe) {
-    //   this.setState({
-    //     changeToAboutMe: !this.changeToAboutMe
-    //   });
-    // }
-    else {
+    } else {
       this.setState({
         changeToSignUp: true
       });
     }
-    //  else {
-    //   this.setState({
-    //     changeToAboutMe: true
-    //   });
-    // }
+    if (this.state.changeToAboutMe) {
+      this.setState({
+        changeToAboutMe: !this.changeToAboutMe
+      });
+    } else {
+      this.setState({
+        changeToAboutMe: true
+      });
+    }
   };
+
   render() {
     let exeComp;
     if (this.state.changeToSignUp) {
       exeComp = (
         //Replace header with new logo image of roomie.
-        <LoginButtons loginRequest={this.changeVisibility} />
-        //<LoggingInForm />
+        <LoginButtons
+          signUpRequest={this.changeVisibility}
+          loginRequest={this.changeToAboutMe}
+        />
       );
     } else {
       if (this.state.changeToAboutMe) {
         exeComp = (
           <SignUpForm
             returnToLogin={this.changeVisibility}
-            changeAboutMe={this.changeVisibility}
+            changeAboutMe={this.changeToAboutMe}
           />
         );
       } else {
