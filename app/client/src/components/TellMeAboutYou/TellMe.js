5ac42a610af76e9306d3c3d9f9789e4401f84671
Jarron <baileyjarron@gmail.com>
2019-04-06 00:40:22 -0500

    Removed material ui components from TellMe Component

--- app/client/src/components/TellMeAboutYou/TellMe.js
+++ app/client/src/components/TellMeAboutYou/TellMe.js
@@ -1,37 +1,12 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 
 export default function TellMe() {
   return (
     <div>
-      <header id='title'> Roomie </header>
+      <header id="title"> Roomie </header>
       <h2>Tell us a bit about you!</h2>
-      <Grid item xs={12}>
-        <TextField
-          id='outlined-textarea'
-          label='About You'
-          placeholder='Use this space to describe yourself... '
-          multiline
-          margin='normal'
-          variant='outlined'
-        />
-      </Grid>
-      <Button
-        variant='outlined'
-        color='secondary'
-        //onClick={props.returnToLogin}
-      >
-        Back
-      </Button>
-      <Button
-        variant='outlined'
-        color='primary'
-        //onClick={props.changeAboutMe}
-      >
-        Continue
-      </Button>
+      <button>{/* onClick={props.returnToLogin} */}Back</button>
+      <button>{/* onClick={props.changeAboutMe} */} Continue</button>
     </div>
   );
 }
