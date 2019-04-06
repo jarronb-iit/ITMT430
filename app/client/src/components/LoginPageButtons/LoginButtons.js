da0aee6172e1858c434cfdf8b6ae5cfd86307475
Jarron <baileyjarron@gmail.com>
2019-04-06 00:40:47 -0500

    Removed material ui components from LoginButtons Component

--- app/client/src/components/LoginPageButtons/LoginButtons.js
+++ app/client/src/components/LoginPageButtons/LoginButtons.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
 
-export default function LoginButtons(props) {
+export default function Loginbuttons(props) {
   return (
     //Replace header with new logo image of roomie.
-    <div id='homePage'>
-      <header id='title'> Roomie </header>
+    <div id="homePage">
+      <header id="title"> Roomie </header>
       {/* 
       <button id='login'>Login</button>
       <button id='signUp' onClick={props.loginRequest}>
@@ -14,16 +12,12 @@ export default function LoginButtons(props) {
       </button> */}
 
       <form>
-        <Grid xs={12}>
-          <Button id='login' onClick={props.loginRequest}>
-            Login
-          </Button>
-        </Grid>
-        <Grid xs={12}>
-          <Button id='signUp' onClick={props.signUpRequest}>
-            Sign Up
-          </Button>
-        </Grid>
+        <button id="login" onClick={props.loginRequest}>
+          Login
+        </button>
+        <button id="signUp" onClick={props.signUpRequest}>
+          Sign Up
+        </button>
       </form>
     </div>
   );
