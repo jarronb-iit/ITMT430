b0a3bc3396a5a8838c421aa3e2a55633aa8c6ec3
Jarron <baileyjarron@gmail.com>
2019-04-06 01:46:34 -0500

    Added css modules for App component

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import "./App.css";
-import Layout from "./Layout/Layout";
-import { loadUserInit } from "./store/actions/authActions";
-import store from "./store";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Styles from './App.module.css';
+import Layout from './Layout/Layout';
+import { loadUserInit } from './store/actions/authActions';
+import store from './store';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
- 
   render() {
     return (
-      <Layout/>
-    )
+      <div className={Styles.App}>
+        <Layout />
+      </div>
+    );
   }
 }
-
-
 
 export default App;
