7fbd710a858dd88498d098ff80a48ac322c71af7
Jarron <baileyjarron@gmail.com>
2019-04-07 05:59:58 -0500

    Imported base input styles and fixed button onClick events

--- app/client/src/components/SignUpForm/SignUpForm.js
+++ app/client/src/components/SignUpForm/SignUpForm.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
 import Styles from './SignUpForm.module.css';
+import BaseStyles from '../../BaseStyles/Input.module.css';
 
 export default function SignUpForm(props) {
-  const handleLogIn = event => {
-    if (this.state.value != 'Antoine');
-    {
-      alert('Your username or password is incorrect');
-      event.preventDefault();
-    }
-  };
+  const inputStyles = [BaseStyles.Input, Styles.Input, 'browser-default'];
+
   return (
-    //Replace header with new logo image of roomie.
-    <div id="homePage">
-      <header id="title"> Roomie </header>
-      <form>
-        <input type="text" id="fullName" placeholder="Full Name" />
-        <input type="text" id="Email" placeholder="Email Address" />
-        <input type="password" id="pass" placeholder="Create a Password" />
-        <input type="password" id="pass" placeholder="Confirm Password" />
-        <input type="text" id="phoneNum" placeholder="Phone Number" />
-        <button onClick={props.returnToLogin}>Back</button>
-        <button onClick={props.changeAboutMe}>Continue</button>
-      </form>
-    </div>
+    <form
+      id="SignUpForm"
+      className={Styles.SignUpForm}
+      onSubmit={props.onSignUpFormSubmit}
+    >
+      <input
+        className={inputStyles.join(' ')}
+        type="text"
+        id="fullName"
+        name="name"
+        placeholder="Full Name"
+      />
+      <input
+        className={inputStyles.join(' ')}
+        type="text"
+        id="Email"
+        placeholder="Email Address"
+      />
+      <input
+        className={inputStyles.join(' ')}
+        type="password"
+        id="pass"
+        placeholder="Create a Password"
+      />
+      <input
+        className={inputStyles.join(' ')}
+        type="password"
+        id="pass"
+        placeholder="Confirm Password"
+      />
+      <input
+        className={inputStyles.join(' ')}
+        type="text"
+        id="phoneNum"
+        placeholder="Phone Number"
+      />
+
+      <button value="showHomepage" onClick={props.changeVisibility}>
+        Back
+      </button>
+      <button value="showAboutMeInfo" onClick={props.changeVisibility}>
+        Continue
+      </button>
+    </form>
   );
 }
