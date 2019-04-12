013e49c72b69d52adcc99e96d8cdd1839107cf88
Jarron <baileyjarron@gmail.com>
2019-04-12 05:33:37 -0500

    Deleted console logs in authSaga

--- app/client/src/store/sagas/authSaga.js
+++ app/client/src/store/sagas/authSaga.js
@@ -18,8 +18,6 @@ export async function tokenConfig() {
 
   // If token then add to headers
   if (token) {
-    console.log('Token stored');
-
     config.headers['x-auth-token'] = token;
   }
 
@@ -27,41 +25,28 @@ export async function tokenConfig() {
 }
 
 export function* loadUserSaga(action) {
-  console.log(axiosInstance);
-
   yield put({ type: actionsTypes.USER_LOADING });
 
   const config = yield tokenConfig();
-  console.log(config);
-
   try {
     const response = yield axiosInstance.get('/api/auth/user', config);
-    yield console.log(response.data);
-
-    yield console.log(response.data);
     const user = yield response.data;
     yield put(actions.loginSuccess(config.headers['x-auth-token'], user));
   } catch (error) {
-    yield console.log(error.response.data.errors);
-    // yield put({ type: actionsTypes.AUTH_ERROR });
-    // yield put(actions.getErrors(error.response.data.errors));
+    yield put({ type: actionsTypes.AUTH_ERROR });
+    yield put(actions.getErrors(error.response.data.errors));
   }
 }
 export function* loginUserSaga(action) {
-  console.log('hererer');
-
   let body = yield {
     email: action.payload.email,
     password: action.payload.password
   };
-  yield console.log('[BODY]', body);
   try {
     const tryPostRes = yield axiosInstance.post('/api/auth', body);
-    yield console.log(tryPostRes.data);
     const { token, user } = yield tryPostRes.data;
     yield put(actions.loginSuccess(token, user));
   } catch (error) {
-    yield console.log(error.response.data.errors);
     yield put({ type: actionsTypes.AUTH_ERROR });
     yield put(actions.getErrors(error.response.data.errors));
   }
