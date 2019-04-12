04b34d4bd2ebfc9d130bcc49876a37f81cf9e650
Jarron <baileyjarron@gmail.com>
2019-04-12 05:29:21 -0500

    Added loginInit to the auth saga and set watcher in root saga

--- app/client/src/store/sagas/authSaga.js
+++ app/client/src/store/sagas/authSaga.js
@@ -1,50 +1,67 @@
-import { put, select } from "redux-saga/effects";
-import axiosInstance from "../../axiosConfig";
-import * as actions from "../actions";
-import * as actionsTypes from "../actions/actionTypes";
+import { put, select, call } from 'redux-saga/effects';
+import axiosInstance from '../../axiosConfig';
+import * as actions from '../actions';
+import * as actionsTypes from '../actions/actionTypes';
 
-export function* tokenConfig() {
-  const state = yield select();
-  console.log(state);
+export async function tokenConfig() {
+  // const state = await select();
+  // console.log(state);
 
   // Get token from storage
-  const token = state.auth.token;
+  const token = localStorage.getItem('token');
   // Headers
   const config = {
     headers: {
-      "Content-type": "application"
+      'Content-type': 'application/json'
     }
   };
 
   // If token then add to headers
   if (token) {
-    yield (config.headers["x-auth-token"] = token);
+    console.log('Token stored');
+
+    config.headers['x-auth-token'] = token;
   }
 
   return config;
 }
+
 export function* loadUserSaga(action) {
-  console.log(action);
+  console.log(axiosInstance);
 
   yield put({ type: actionsTypes.USER_LOADING });
 
-  // const config = tokenConfig().next().value;
-  // console.log("TOKEN", config);
-  try {
-    // const response = yield axiosInstance.get("/api/auth/user", config);
-    // console.log(response.data);
-    let body = {
-      email: "baileyjarron@gmail.com",
-      password: "password"
-    };
+  const config = yield tokenConfig();
+  console.log(config);
 
-    const tryPostRes = yield axiosInstance.post("/api/auth", body);
-    console.log(tryPostRes.data);
-    const { token, user } = tryPostRes.data;
-    yield put(actions.loginSuccess(token, user));
-    yield put(actions.loadUserSuccess());
+  try {
+    const response = yield axiosInstance.get('/api/auth/user', config);
+    yield console.log(response.data);
+
+    yield console.log(response.data);
+    const user = yield response.data;
+    yield put(actions.loginSuccess(config.headers['x-auth-token'], user));
   } catch (error) {
-    console.log(error.response.data.errors);
+    yield console.log(error.response.data.errors);
+    // yield put({ type: actionsTypes.AUTH_ERROR });
+    // yield put(actions.getErrors(error.response.data.errors));
+  }
+}
+export function* loginUserSaga(action) {
+  console.log('hererer');
+
+  let body = yield {
+    email: action.payload.email,
+    password: action.payload.password
+  };
+  yield console.log('[BODY]', body);
+  try {
+    const tryPostRes = yield axiosInstance.post('/api/auth', body);
+    yield console.log(tryPostRes.data);
+    const { token, user } = yield tryPostRes.data;
+    yield put(actions.loginSuccess(token, user));
+  } catch (error) {
+    yield console.log(error.response.data.errors);
     yield put({ type: actionsTypes.AUTH_ERROR });
     yield put(actions.getErrors(error.response.data.errors));
   }
