ddfad0f345f790ad1d1d930ac67e900f3ae04563
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-17 23:30:56 -0500

    create usertype page for final submit

---
+++ app/client/src/components/UserType/UserType.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Styles from '../SignUpForm/SignUpForm.module.css';
+import BaseStyles from '../../BaseStyles/Input.module.css';
+
+export default function UserType(props) {
+  const inputStyles = [BaseStyles.Input, Styles.Input, 'browser-default'];
+
+  return (
+    <form
+      id='userType'
+      className={Styles.SignUpForm}
+      onSubmit={props.onSignUpFormSubmit}
+    >
+      <h1>Im Looking for property</h1>
+      <h1>Im selling property</h1>
+      <button value='showHomepage' onClick={props.changeVisibility}>
+        Back
+      </button>
+      <button value='showAboutMeInfo' onClick={props.changeVisibility}>
+        Continue
+      </button>
+    </form>
+  );
+}
