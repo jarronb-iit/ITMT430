3bcced63d8fe0494048f260b6584406414def860
Jarron <baileyjarron@gmail.com>
2019-04-18 13:46:38 -0500

    Created register init action type

--- app/client/src/store/actions/actionTypes.js
+++ app/client/src/store/actions/actionTypes.js
@@ -10,6 +10,7 @@ export const LOGIN_INIT = 'LOGIN_INIT';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const REGISTER_INIT = 'REGISTER_INIT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const GET_ERRORS = 'GET_ERRORS';
