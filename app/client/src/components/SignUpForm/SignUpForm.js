d9d47c018df6e8c16312053c606160cf70e686bb
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-17 23:43:07 -0500

    add button functionality to Sign Up form

--- app/client/src/components/SignUpForm/SignUpForm.js
+++ app/client/src/components/SignUpForm/SignUpForm.js
@@ -1,54 +1,54 @@
 import React, { Component } from 'react';
 import Styles from './SignUpForm.module.css';
+import { NavLink } from 'react-router-dom';
 import BaseStyles from '../../BaseStyles/Input.module.css';
 
 export default function SignUpForm(props) {
   const inputStyles = [BaseStyles.Input, Styles.Input, 'browser-default'];
-
+  const signUpBtnClasses = [BaseStyles.Button, Styles.SignUpBtn];
   return (
     <form
-      id="SignUpForm"
+      id='SignUpForm'
       className={Styles.SignUpForm}
       onSubmit={props.onSignUpFormSubmit}
     >
       <input
         className={inputStyles.join(' ')}
-        type="text"
-        id="fullName"
-        name="name"
-        placeholder="Full Name"
+        type='text'
+        id='fullName'
+        name='name'
+        placeholder='Full Name'
       />
       <input
         className={inputStyles.join(' ')}
-        type="text"
-        id="Email"
-        placeholder="Email Address"
+        type='text'
+        id='Email'
+        placeholder='Email Address'
       />
       <input
         className={inputStyles.join(' ')}
-        type="password"
-        id="pass"
-        placeholder="Create a Password"
+        type='password'
+        id='pass'
+        placeholder='Create a Password'
       />
       <input
         className={inputStyles.join(' ')}
-        type="password"
-        id="pass"
-        placeholder="Confirm Password"
+        type='password'
+        id='pass'
+        placeholder='Confirm Password'
       />
       <input
         className={inputStyles.join(' ')}
-        type="text"
-        id="phoneNum"
-        placeholder="Phone Number"
+        type='text'
+        id='phoneNum'
+        placeholder='Phone Number'
       />
-
-      <button value="showHomepage" onClick={props.changeVisibility}>
-        Back
-      </button>
-      <button value="showAboutMeInfo" onClick={props.changeVisibility}>
+      <NavLink className={BaseStyles.Button} to='/aboutme' value='/showAboutMe'>
         Continue
-      </button>
+      </NavLink>
+      <NavLink className={signUpBtnClasses.join(' ')} to='/' value='return'>
+        Back
+      </NavLink>
     </form>
   );
 }
