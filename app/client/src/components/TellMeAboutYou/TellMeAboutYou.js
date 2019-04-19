834f3bb3145a5c4f09721ee34bbd683cb9424fde
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-19 02:02:39 -0500

    convert function to class for state manipulation

--- app/client/src/components/TellMeAboutYou/TellMeAboutYou.js
+++ app/client/src/components/TellMeAboutYou/TellMeAboutYou.js
@@ -5,39 +5,40 @@ import ButtonBaseStyles from '../../BaseStyles/Button.module.css';
 import HomeStyles from '../../Pages/Home/Home.module.css';
 import { NavLink } from 'react-router-dom';
 
-export default function TellMe(props) {
-  const continueButtonStyles = [BaseStyles.Button, HomeStyles.Button1];
-  const backButtonStyles = [BaseStyles.Button, HomeStyles.Button2];
-  return (
-    <div>
+export default class TellMe extends Component {
+  render() {
+    const continueButtonStyles = [BaseStyles.Button, HomeStyles.Button1];
+    const backButtonStyles = [BaseStyles.Button, HomeStyles.Button2];
+    return (
+      <div>
+        <h2 className={Styles.h2}>Tell us a bit about you!</h2>
 
-      <h2 className = {Styles.h2}>Tell us a bit about you!</h2>
+        <form className={Styles.form}>
+          <textarea
+            className={Styles.input}
+            cols='20'
+            rows='50'
+            placeholder='I am pretty cool...'
+          />
+        </form>
 
-      <form className ={Styles.form}>
-      <textarea className={Styles.input} cols="20" rows="50" placeholder="I am pretty cool..."></textarea>
-
-
-      </form>
-
-      <section className={HomeStyles.container}>
-
-      <NavLink
-        className={continueButtonStyles.join(" ")}
-        to='/usertype'
-        value='/showAboutMe'
-      >
-        Continue
-      </NavLink>
-      <NavLink
-        className={backButtonStyles.join(" ")}
-        to='/signup'
-        value='return'
-      >
-        Back
-      </NavLink>
-
-      </section>
-
-    </div>
-  );
+        <section className={HomeStyles.container}>
+          <NavLink
+            className={continueButtonStyles.join(' ')}
+            to='/usertype'
+            value='/showAboutMe'
+          >
+            Continue
+          </NavLink>
+          <NavLink
+            className={backButtonStyles.join(' ')}
+            to='/signup'
+            value='return'
+          >
+            Back
+          </NavLink>
+        </section>
+      </div>
+    );
+  }
 }
