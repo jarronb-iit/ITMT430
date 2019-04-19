b9e99fb6e34a026677dc6a4427a17155bb504f55
Theo Economos <theoeconomos1@gmail.com>
2019-04-19 01:25:13 -0500

    added usertype design

--- app/client/src/components/UserType/UserType.js
+++ app/client/src/components/UserType/UserType.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import Styles from '../SignUpForm/SignUpForm.module.css';
 import BaseStyles from '../../BaseStyles/Input.module.css';
+import HomeStyles from '../../Pages/Home/Home.module.css';
+import TypeStyles from './UserType.module.css';
 import { NavLink } from 'react-router-dom';
 
 export default function UserType(props) {
   const inputStyles = [BaseStyles.Input, Styles.Input, 'browser-default'];
-  const signUpBtnClasses = [BaseStyles.Button, Styles.SignUpBtn];
+  const continueButtonStyles = [BaseStyles.Button, HomeStyles.Button1];
+  const backButtonStyles = [BaseStyles.Button, HomeStyles.Button2];
   return (
     <form
       id='userType'
       className={Styles.SignUpForm}
       onSubmit={props.onSignUpFormSubmit}
     >
-      <h1>Im Looking for property</h1>
-      <h1>Im selling property</h1>
-      <NavLink className={BaseStyles.Button} to='/aboutme' value='/showAboutMe'>
-        Back
-      </NavLink>
-      <NavLink className={signUpBtnClasses.join(' ')} to='#' value='COMPLETE'>
+      <h1 className={TypeStyles.h1}>I'm Looking for property</h1>
+      <h1 className={TypeStyles.h2}>I'm selling property</h1>
+
+      <section className={HomeStyles.container}>
+
+      
+      <NavLink className={continueButtonStyles.join(" ")} to='#' value='COMPLETE'>
         FINISH
       </NavLink>
+      <NavLink className={backButtonStyles.join(" ")} to='/aboutme' value='/showAboutMe'>
+        Back
+      </NavLink>
+
+      </section>
+
     </form>
   );
 }
