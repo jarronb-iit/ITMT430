45689d2858da2b1cb6e9f88031733d27d08ae7d3
Theo Economos <theoeconomos1@gmail.com>
2019-04-19 01:01:59 -0500

    designed css layout for about me section

--- app/client/src/components/TellMeAboutYou/TellMeAboutYou.js
+++ app/client/src/components/TellMeAboutYou/TellMeAboutYou.js
@@ -2,29 +2,42 @@ import React, { Component } from 'react';
 import Styles from './TellMeAboutYou.module.css';
 import BaseStyles from '../../BaseStyles/Input.module.css';
 import ButtonBaseStyles from '../../BaseStyles/Button.module.css';
+import HomeStyles from '../../Pages/Home/Home.module.css';
 import { NavLink } from 'react-router-dom';
 
 export default function TellMe(props) {
-  const loginStyleClasses = [ButtonBaseStyles.Button, Styles.SignUpBtn];
-  const loginButtonClasses = [ButtonBaseStyles.Button, ButtonBaseStyles.Blue];
+  const continueButtonStyles = [BaseStyles.Button, HomeStyles.Button1];
+  const backButtonStyles = [BaseStyles.Button, HomeStyles.Button2];
   return (
     <div>
 
       <h2 className = {Styles.h2}>Tell us a bit about you!</h2>
+
+      <form className ={Styles.form}>
+      <textarea className={Styles.input} cols="20" rows="50" placeholder="I am pretty cool..."></textarea>
+
+
+      </form>
+
+      <section className={HomeStyles.container}>
+
       <NavLink
-        className={loginButtonClasses.join(' ')}
+        className={continueButtonStyles.join(" ")}
         to='/usertype'
         value='/showAboutMe'
       >
         Continue
       </NavLink>
       <NavLink
-        className={loginStyleClasses.join(' ')}
+        className={backButtonStyles.join(" ")}
         to='/signup'
         value='return'
       >
         Back
       </NavLink>
+
+      </section>
+
     </div>
   );
 }
