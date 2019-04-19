8c9ad72f5756e87cc0cd8045f2ec0346aa656fbb
Jarron <baileyjarron@gmail.com>
2019-04-19 06:12:33 -0500

    Created a switch for the layout routeing and addded a protected "/home" route

--- app/client/src/Layout/Layout.js
+++ app/client/src/Layout/Layout.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Home from '../Pages/Home/Home';
+import Index from '../Pages/Index/Index';
 import Styles from '../Pages/Home/Home.module.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import ProtectedRoute from '../hoc/ProtectedRoute';
 
 // import { Route, Switch } from "react-router-dom";
 
 export default class Layout extends Component {
   render() {
-    return <Route path="/" component={Home} />;
+    return (
+      <Switch>
+        <ProtectedRoute path="/home" component={Index} />
+        <Route path="/" component={Home} />
+      </Switch>
+    );
   }
 }
