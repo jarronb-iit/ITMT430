340362d3cc03941537a9ff7bc2caf89d8a2c892f
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-04-19 02:18:37 -0500

    switch usertype to class for sending states

--- app/client/src/components/UserType/UserType.js
+++ app/client/src/components/UserType/UserType.js
@@ -5,31 +5,33 @@ import HomeStyles from '../../Pages/Home/Home.module.css';
 import TypeStyles from './UserType.module.css';
 import { NavLink } from 'react-router-dom';
 
-export default function UserType(props) {
-  const inputStyles = [BaseStyles.Input, Styles.Input, 'browser-default'];
-  const continueButtonStyles = [BaseStyles.Button, HomeStyles.Button1];
-  const backButtonStyles = [BaseStyles.Button, HomeStyles.Button2];
-  return (
-    <form
-      id='userType'
-      className={Styles.SignUpForm}
-      onSubmit={props.onSignUpFormSubmit}
-    >
-      <h1 className={TypeStyles.h1}>I'm Looking for property</h1>
-      <h1 className={TypeStyles.h2}>I'm selling property</h1>
+export default class UserType extends Component {
+  render() {
+    const inputStyles = [BaseStyles.Input, Styles.Input, 'browser-default'];
+    const continueButtonStyles = [BaseStyles.Button, HomeStyles.Button1];
+    const backButtonStyles = [BaseStyles.Button, HomeStyles.Button2];
+    return (
+      <form id='userType' className={Styles.SignUpForm}>
+        <h1 className={TypeStyles.h1}>I'm Looking for property</h1>
+        <h1 className={TypeStyles.h2}>I'm selling property</h1>
 
-      <section className={HomeStyles.container}>
-
-      
-      <NavLink className={continueButtonStyles.join(" ")} to='#' value='COMPLETE'>
-        FINISH
-      </NavLink>
-      <NavLink className={backButtonStyles.join(" ")} to='/aboutme' value='/showAboutMe'>
-        Back
-      </NavLink>
-
-      </section>
-
-    </form>
-  );
+        <section className={HomeStyles.container}>
+          <NavLink
+            className={continueButtonStyles.join(' ')}
+            to='#'
+            value='COMPLETE'
+          >
+            FINISH
+          </NavLink>
+          <NavLink
+            className={backButtonStyles.join(' ')}
+            to='/aboutme'
+            value='/showAboutMe'
+          >
+            Back
+          </NavLink>
+        </section>
+      </form>
+    );
+  }
 }
