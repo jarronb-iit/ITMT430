a9a1cd0bd84ffd8d33fdd4e265712e1f4321360f
Jarron <baileyjarron@gmail.com>
2019-04-22 17:21:04 -0500

    Created admin route to get all users

--- app/server/routes/api/admin.js
+++ app/server/routes/api/admin.js
@@ -16,4 +16,26 @@ router.get('/test', (req, res) => {
     msg: 'Admin works'
   });
 });
+
+// @route   GET api/admin/users
+// @desc    Get all users
+// @access  Private
+router.get('/users', auth, (req, res) => {
+  let { roles } = req.user;
+  let role = roles.find(role => role === 'admin');
+  if (role !== 'admin') {
+    return res.status(401).json({
+      errors: [{ message: 'Not authorized' }]
+    });
+  }
+
+  User.find({})
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      const errors = errorsFormatter(err);
+      return res.status(400).json({ errors: errors });
+    });
+});
 module.exports = router;
