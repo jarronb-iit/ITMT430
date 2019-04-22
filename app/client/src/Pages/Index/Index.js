2a16dab93db3e677878bc158f3bef2ec13ab404c
Theo Economos <theoeconomos1@gmail.com>
2019-04-21 21:03:58 -0500

    added further layout for full page

--- app/client/src/Pages/Index/Index.js
+++ app/client/src/Pages/Index/Index.js
@@ -31,54 +31,57 @@ export default class Index extends Component {
 
           {/* Card 2 */}
           <div className="col s12 m6">
-            <div className="card blue-grey darken-1">
-              <div className="card-content white-text">
-                <span className={Styles.cardTitle}>Listing 2</span>
-                <p>
-                  I am a very simple card. I am good at containing small bits of
-                  information. I am convenient because I require little markup
-                  to use effectively.
-                </p>
+            <div className="card white darken-1">
+              <div className="card-content cyan-text">
+              <div className={Styles.cardPhoto}>
               </div>
+              </div>
+                <span className={Styles.cardTitle}>10 Sunset Drive</span>
+                <div className={Styles.yo}>
+                <span className={Styles.cardAddress}>10 Sunset Drive, Malibu, CA</span>
+                <span className={Styles.cardPrice}> $2,000 </span>
+                </div>
               <div className="card-action">
-                <a href="#">This is a link</a>
-                <a href="#">This is a link</a>
+                <span className={Styles.cardBedBath}>3 Bed 3 Bath</span>
+                <span className={Styles.cardSqft}>2300 Sqft</span>
               </div>
             </div>
           </div>
 
           {/* Card 3 */}
           <div className="col s12 m6">
-            <div className="card blue-grey darken-1">
-              <div className="card-content white-text">
-                <span className={Styles.cardTitle}>Listing 3</span>
-                <p>
-                  I am a very simple card. I am good at containing small bits of
-                  information. I am convenient because I require little markup
-                  to use effectively.
-                </p>
+            <div className="card white darken-1">
+              <div className="card-content cyan-text">
+              <div className={Styles.cardPhoto}>
               </div>
+              </div>
+                <span className={Styles.cardTitle}>10 Sunset Drive</span>
+                <div className={Styles.yo}>
+                <span className={Styles.cardAddress}>10 Sunset Drive, Malibu, CA</span>
+                <span className={Styles.cardPrice}> $2,000 </span>
+                </div>
               <div className="card-action">
-                <a href="#">This is a link</a>
-                <a href="#">This is a link</a>
+                <span className={Styles.cardBedBath}>3 Bed 3 Bath</span>
+                <span className={Styles.cardSqft}>2300 Sqft</span>
               </div>
             </div>
           </div>
 
           {/* Card 4 */}
           <div className="col s12 m6">
-            <div className="card blue-grey darken-1">
-              <div className="card-content white-text">
-                <span className={Styles.cardTitle}>Listing 4</span>
-                <p>
-                  I am a very simple card. I am good at containing small bits of
-                  information. I am convenient because I require little markup
-                  to use effectively.
-                </p>
+            <div className="card white darken-1">
+              <div className="card-content cyan-text">
+              <div className={Styles.cardPhoto}>
               </div>
+              </div>
+                <span className={Styles.cardTitle}>10 Sunset Drive</span>
+                <div className={Styles.yo}>
+                <span className={Styles.cardAddress}>10 Sunset Drive, Malibu, CA</span>
+                <span className={Styles.cardPrice}> $2,000 </span>
+                </div>
               <div className="card-action">
-                <a href="#">This is a link</a>
-                <a href="#">This is a link</a>
+                <span className={Styles.cardBedBath}>3 Bed 3 Bath</span>
+                <span className={Styles.cardSqft}>2300 Sqft</span>
               </div>
             </div>
           </div>
