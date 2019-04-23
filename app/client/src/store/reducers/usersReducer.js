ccc6cc39ab6bef503ae634dc90cf67246cafaee4
Jarron <baileyjarron@gmail.com>
2019-04-23 03:09:21 -0500

    Added viewingUser to the users reducer

--- app/client/src/store/reducers/usersReducer.js
+++ app/client/src/store/reducers/usersReducer.js
@@ -1,7 +1,8 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-  users: []
+  users: [],
+  viewingUser: {}
 };
 
 const getUsers = (state, action) => {
@@ -12,10 +13,30 @@ const getUsers = (state, action) => {
   return state;
 };
 
+const getUser = (state, action) => {
+  state = {
+    ...state,
+    viewingUser: action.payload.user
+  };
+  return state;
+};
+
+const clearUser = (state, action) => {
+  state = {
+    ...state,
+    viewingUser: {}
+  };
+  return state;
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.GET_USERS_SUCCESS:
       return getUsers(state, action);
+    case actionTypes.GET_USER_SUCCESS:
+      return getUser(state, action);
+    case actionTypes.CLEAR_USER:
+      return clearUser(state, action);
     default:
       return state;
   }
