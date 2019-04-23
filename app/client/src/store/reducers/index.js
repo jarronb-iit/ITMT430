429a9338b6f9d32c3ef8f4a1f5eb7c36416013f0
Jarron <baileyjarron@gmail.com>
2019-04-22 21:18:51 -0500

    Added lisitngs reducer to hold its state

--- app/client/src/store/reducers/index.js
+++ app/client/src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import errorsReducer from './errorsReducer';
 import authReducer from './authReducer';
+import listingsReducer from './listingsReducer';
 
 export default combineReducers({
   errors: errorsReducer,
-  auth: authReducer
+  auth: authReducer,
+  listings: listingsReducer
 });
---
+++ app/client/src/store/reducers/listingsReducer.js
@@ -0,0 +1,25 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  listings: []
+};
+
+const getListings = (state, action) => {
+  console.log('HERE', action);
+  state = {
+    ...state,
+    listings: action.payload.listings
+  };
+  return state;
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.GET_LISTINGS_SUCCESS:
+      return getListings(state, action);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
