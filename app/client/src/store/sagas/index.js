f5186e0da301d2879c3d611a309e53acadee9014
Jarron <baileyjarron@gmail.com>
2019-04-22 21:20:30 -0500

    Created an listings saga to get all listings

--- app/client/src/store/sagas/index.js
+++ app/client/src/store/sagas/index.js
@@ -1,10 +1,18 @@
 import { takeEvery, take, takeLatest } from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import * as authSaga from './authSaga';
+import * as listingsSaga from './listingsSaga';
 
 export function* watchAuth() {
   yield takeLatest(actionTypes.USER_LOAD_INIT, authSaga.loadUserSaga);
   yield takeLatest(actionTypes.LOGIN_INIT, authSaga.loginUserSaga);
   yield takeLatest(actionTypes.REGISTER_INIT, authSaga.registerUserSaga);
   yield takeLatest(actionTypes.DELETE_USER_INIT, authSaga.deleteUserSaga);
+}
+
+export function* watchListings() {
+  yield takeLatest(
+    actionTypes.GET_LISTINGS_INIT,
+    listingsSaga.loadListingsSaga
+  );
 }
---
+++ app/client/src/store/sagas/listingsSaga.js
@@ -0,0 +1,38 @@
+import { put, select, call } from 'redux-saga/effects';
+import axiosInstance from '../../axiosConfig';
+import * as actions from '../actions';
+import * as actionsTypes from '../actions/actionTypes';
+import store from '../../store';
+
+export async function tokenConfig() {
+  const state = await store.getState();
+
+  // Get token from storage
+  const token = state.auth.token;
+  // Headers
+  const config = {
+    headers: {
+      'Content-type': 'application/json'
+    }
+  };
+
+  // If token then add to headers
+  if (token) {
+    config.headers['x-auth-token'] = token;
+  }
+
+  return config;
+}
+
+export function* loadListingsSaga(action) {
+  const config = yield tokenConfig();
+  try {
+    const response = yield axiosInstance.get('/api/listings', config);
+    const listings = yield response.data;
+    console.log(listings);
+
+    yield put(actions.getListingsSuccess(listings));
+  } catch (error) {
+    yield put(actions.getErrors(error.response.data.errors));
+  }
+}
