52a37cb9d2655030c4778e353f8a4173d8425ff7
Jarron <baileyjarron@gmail.com>
2019-04-24 17:32:18 -0500

    Added material ui them to app component

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -6,14 +6,22 @@ import Layout from './Layout/Layout';
 import { loadUserInit } from './store/actions/authActions';
 import store from './store';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
+import theme from './App.MuiTheme';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
+console.log(theme);
 
 store.dispatch(loadUserInit());
 
 class App extends Component {
   render() {
     return (
-      <div className={Styles.App}>
-        <Layout />
+      <div>
+        <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout />
+        </MuiThemeProvider>
       </div>
     );
   }
