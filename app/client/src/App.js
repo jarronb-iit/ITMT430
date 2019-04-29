ca2933c458000da0e19ad073e0da81417213c087
Jarron <baileyjarron@gmail.com>
2019-04-29 02:00:54 -0500

    Removed unused imports App.js

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import Styles from './App.module.css';
 import Layout from './Layout/Layout';
 import { loadUserInit } from './store/actions/authActions';
 import store from './store';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import theme from './App.MuiTheme';
 import { MuiThemeProvider } from '@material-ui/core/styles';
