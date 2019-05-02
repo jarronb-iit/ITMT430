ad509f354b46d598ef5d59c1c72dcd410d9eee36
Jarron <baileyjarron@gmail.com>
2019-05-02 03:02:11 -0500

    Switched protected routes to be protected

--- app/client/src/Layout/index.js
+++ app/client/src/Layout/index.js
@@ -7,7 +7,7 @@ import AdminPage from '../Pages/AdminPage';
 import HomePage from '../Pages/HomePage';
 import LogoutPage from '../Pages/LogoutPage';
 import { Route, Switch, Redirect } from 'react-router-dom';
-// import ProtectedRoute from '../hoc/ProtectedRoute';
+import ProtectedRoute from '../hoc/ProtectedRoute';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -25,10 +25,10 @@ class Layout extends Component {
         <Switch>
           <Route path="/login" component={LoginPage} />
           <Route path="/signup" component={SignupPage} />
-          <Route path="/createListing" component={CreateListingPage} />
-          <Route path="/admin" component={AdminPage} />
-          <Route path="/home" component={HomePage} />
-          <Route path="/logout" component={LogoutPage} />
+          <ProtectedRoute path="/createListing" component={CreateListingPage} />
+          <ProtectedRoute path="/admin" component={AdminPage} />
+          <ProtectedRoute path="/home" component={HomePage} />
+          <ProtectedRoute path="/logout" component={LogoutPage} />
           <Route path="/" exact component={IndexPage} />
           <Route path="/" component={() => <Redirect to="/" />} />
         </Switch>
