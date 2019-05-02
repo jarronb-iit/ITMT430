9e4c52a5402761161cc7fcfcdbc9944937ee2b75
Jarron <baileyjarron@gmail.com>
2019-05-02 14:48:32 -0500

    Moved BrowserRouter to AppComp and retrieved suth state in AppComp, which is passed to layout

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
 import Layout from './Layout/';
-import { loadUserInit } from './store/actions/authActions';
-import store from './store';
 import { CssBaseline } from '@material-ui/core';
 import theme from './App.MuiTheme';
 import { MuiThemeProvider } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import * as actions from './store/actions';
+import { BrowserRouter } from 'react-router-dom';
 
-console.log(theme);
-
-store.dispatch(loadUserInit());
+// console.log(theme);
 
 class App extends Component {
+  componentDidMount() {
+    this.props.loadUser();
+  }
+
   render() {
+    const { auth } = this.props;
+    let renderedComp = '';
+
+    if (auth.done) {
+      renderedComp = <Layout auth={auth} />;
+    }
+
     return (
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <Layout />
+        <BrowserRouter>{renderedComp}</BrowserRouter>
       </MuiThemeProvider>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    loadUser: () => dispatch(actions.loadUserInit())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
