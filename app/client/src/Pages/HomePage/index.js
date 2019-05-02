6396bbbcba4f9522355d3654c0183bcf55a2acda
Jarron <baileyjarron@gmail.com>
2019-05-02 01:44:45 -0500

    Passed history object from homepage to In app layout

--- app/client/src/Pages/HomePage/index.js
+++ app/client/src/Pages/HomePage/index.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import LayoutInApp from '../../Layout/LayoutInApp';
 
 export default class index extends Component {
+  state = {};
   render() {
+    const { history } = this.props;
     return (
-      <LayoutInApp>
+      <LayoutInApp history={history}>
         <div>hey</div>
       </LayoutInApp>
     );
