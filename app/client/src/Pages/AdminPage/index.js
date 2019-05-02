dbb00859d0e3f669bde975a56ba947755696a5df
Jarron <baileyjarron@gmail.com>
2019-05-02 16:09:23 -0500

    Created USer Data Table View for admin

--- app/client/src/Pages/AdminPage/index.js
+++ app/client/src/Pages/AdminPage/index.js
@@ -1,25 +1,110 @@
-import React from 'react';
-import MUIDataTable from 'mui-datatables';
+import React, { Component } from 'react';
+import LayoutInApp from '../../Layout/LayoutInApp';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions';
+import UsersDataTable from './UsersDataTable';
+import {
+  withStyles,
+  createMuiTheme,
+  MuiThemeProvider
+} from '@material-ui/core/styles';
 
-const columns = ['Name', 'Company', 'City', 'State'];
+const styles = theme => ({
+  root: {
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+});
 
-const data = [
-  ['Joe James', 'Test Corp', 'Yonkers', 'NY'],
-  ['John Walsh', 'Test Corp', 'Hartford', 'CT'],
-  ['Bob Herm', 'Test Corp', 'Tampa', 'FL'],
-  ['James Houston', 'Test Corp', 'Dallas', 'TX']
-];
+class ReactAdmin extends Component {
+  state = {};
 
-const options = {
-  filterType: 'checkbox'
+  getMuiTheme = () =>
+    createMuiTheme({
+      overrides: {
+        MUIDataTable: {
+          root: {
+            backgroundColor: '#FF000',
+            overflowX: 'scroll',
+            position: 'static',
+            width: '100vh'
+          },
+          paper: {
+            boxShadow: 'none'
+          }
+        },
+        MUIDataTableBodyCell: {
+          root: {
+            // backgroundColor: '#FF0000'
+          },
+          responsiveStacked: {
+            '@media screen and (min-width: 100px)': {
+              border: 'solid 2px rgba(0, 0, 0, 0.15)'
+            }
+          }
+        },
+        MUIDataTableFilterList: {
+          '& + div': {
+            overflow: 'scroll',
+            position: 'static'
+          }
+        }
+      }
+    });
+
+  componentDidMount() {
+    this.props.getUsers();
+    this.props.getListings();
+  }
+
+  static getDerivedStateFromProps(props, state) {}
+
+  transformUsersData = users => {
+    return users.map(user => {
+      return [user._id, user.email];
+    });
+  };
+
+  rowsDeleted = rowsDeleted => {
+    console.log(rowsDeleted);
+  };
+
+  render() {
+    let { history, users, listings } = this.props;
+    let data;
+
+    if (users) {
+      // users = this.transformUsersData(users);
+      data = users;
+    }
+
+    return (
+      <LayoutInApp history={history}>
+        <MuiThemeProvider theme={this.getMuiTheme()}>
+          <UsersDataTable data={data} />
+        </MuiThemeProvider>
+      </LayoutInApp>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    listings: state.listings.listings,
+    users: state.users.users
+  };
 };
-const ReactAdmin = () => (
-  <MUIDataTable
-    title={'Employee List'}
-    data={data}
-    columns={columns}
-    options={options}
-  />
-);
 
-export default ReactAdmin;
+const mapDispatchToProps = dispatch => {
+  return {
+    getUsers: () => dispatch(actions.getUsersInit()),
+    getListings: () => dispatch(actions.getListingsInit())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(ReactAdmin));
