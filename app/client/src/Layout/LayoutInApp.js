b3363fc72da352248ca7da611bcf218f57ba826d
Jarron <baileyjarron@gmail.com>
2019-05-02 01:44:12 -0500

    Created Drawer componet and integrated the AppBar and Drawer componet in the "InAppLayout" component

--- app/client/src/Layout/LayoutInApp.js
+++ app/client/src/Layout/LayoutInApp.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppBar from '../components/UI/AppBar';
+import Drawer from '../components/UI/Drawer';
+import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
 
-function LayoutInApp(props) {
-  return <div>{props.children}</div>;
+const styles = theme => ({
+  root: {
+    minHeight: '100vh'
+  }
+});
+
+class LayoutInApp extends Component {
+  state = {
+    showDrawer: false
+  };
+
+  onMenuClick = () => {};
+
+  toggleDrawer = () => {
+    this.setState({
+      showDrawer: !this.state.showDrawer
+    });
+  };
+
+  onDrawerClose = () => {
+    this.setState({
+      showDrawer: false
+    });
+  };
+
+  onDrawerOpen = () => {
+    this.setState({
+      showDrawer: true
+    });
+  };
+
+  render() {
+    const { classes, children, history } = this.props;
+    const { showDrawer } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar toggleDrawer={this.toggleDrawer} />
+        <Drawer
+          history={history}
+          showDrawer={showDrawer}
+          onOpen={this.onDrawerOpen}
+          onClose={this.onDrawerClose}
+        />
+        {children}
+      </div>
+    );
+  }
 }
 
-export default LayoutInApp;
+export default withStyles(styles)(LayoutInApp);
