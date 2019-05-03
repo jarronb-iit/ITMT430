8486775981ea16ff92de56ff650d049d6e03ca9a
Jarron <baileyjarron@gmail.com>
2019-05-03 02:50:24 -0500

    Updated production build

---
+++ app/client/build/precache-manifest.1613ceb539442cc8c1aedd773855518d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd6bb521b7fbaa2ad127",
+    "url": "/static/js/main.2f0ae152.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e82e952ae5d367983770",
+    "url": "/static/js/2.1edbcf78.chunk.js"
+  },
+  {
+    "revision": "21f6cf17a0610a964a1563bd7b3cc3e5",
+    "url": "/static/media/bedroom.21f6cf17.jpg"
+  },
+  {
+    "revision": "58ccb42b5b554f6ba3af10df8ca4908a",
+    "url": "/index.html"
+  }
+];
