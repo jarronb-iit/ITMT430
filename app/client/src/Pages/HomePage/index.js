b33700b63fcc80cc0c5e0938da499e481d4ba926
giancarlobarillas <35548676+giancarlobarillas@users.noreply.github.com>
2019-05-02 23:05:26 -0500

    apply logic to generate listview dynamically

--- app/client/src/Pages/HomePage/index.js
+++ app/client/src/Pages/HomePage/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import LayoutInApp from '../../Layout/LayoutInApp';
 import ListingView from '../ListingView/ListingView';
+import * as actions from '../../store/actions';
+import { connect } from 'react-redux';
 
 const styles = theme => ({
   root: {
@@ -15,18 +17,38 @@ const styles = theme => ({
 
 class HomePage extends Component {
   state = {};
+  componentDidMount() {
+    // this.props.getUsers();
+    this.props.getListings();
+  }
   render() {
+    const array = this.props.listings;
     const { history } = this.props;
-
     return (
       <LayoutInApp history={history}>
-        <ListingView />
-        <ListingView />
-        <ListingView />
-        <ListingView />
+        {array.map((item, index) => (
+          <ListingView data={array[index]} />
+        ))}
       </LayoutInApp>
     );
   }
 }
 
-export default withStyles(styles)(HomePage);
+const mapStateToProps = state => {
+  return {
+    listings: state.listings.listings,
+    users: state.users.users
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getUsers: () => dispatch(actions.getUsersInit()),
+    getListings: () => dispatch(actions.getListingsInit())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(HomePage));
