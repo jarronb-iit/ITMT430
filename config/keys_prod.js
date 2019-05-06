6cfade9d5d85e89e3ee1add07e165202a2df5c96
Jarron <baileyjarron@gmail.com>
2019-05-05 22:33:54 -0500

    Adjust scripts and keys for developments

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,3 @@
-const dotenvJSON = require('dotenv-json');
-dotenvJSON({ path: '../../packer/vanilla-install/variables.json' });
-
 module.exports = {
   mongoURI: process.env['database-production-connection'],
   nginxIP: process.env['nginx-web-server-ip'],
